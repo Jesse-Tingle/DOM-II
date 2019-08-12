a6dda562cf41add81240da6ada52d21deaf2575b
Jesse-Tingle <jtingle0@gmail.com>
2019-08-11 22:27:12 -0400

    add mouseover event listener to change background color on h1 in header to yellow , add click event listener to increase size of images in content section when clicked

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+let header = document.querySelector('h1');
+document.addEventListener('mouseover', () => {
+    header.style.backgroundColor = '#FDE45B';
+})
+
+
+let images = document.querySelectorAll('.img-content');
+for (let image of images) {
+    image.addEventListener('click', () => {
+        image.style.width = '50%';
+    })
+}
