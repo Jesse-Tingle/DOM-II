d838e8a7e82adab7e2f02b03e980f8a7e7c8fef4
Jesse-Tingle <jtingle0@gmail.com>
2019-08-12 01:40:48 -0400

    add mousedown event listener and mouseleave event listener

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 // Your code goes here
+
+// adds a background-color of yellow to the
+// h1 tag in the header
 let header = document.querySelector('h1');
 document.addEventListener('mouseover', () => {
     header.style.backgroundColor = '#FDE45B';
 })
 
-
+// increases the image size of the two images
+// in the content section
 let images = document.querySelectorAll('.img-content');
 for (let image of images) {
     image.addEventListener('click', () => {
@@ -12,14 +16,26 @@ for (let image of images) {
     })
 }
 
+// alerts a message notifying the user that 
+// the text was coppied to the clipboard
 let allText = document.querySelector('body');
 document.addEventListener('copy', () => {
     alert('Text was coppied!');
 })
 
+// change button text color to red on mousedown
+let buttons = document.querySelectorAll('.btn');
+for(let button of buttons) {
+    button.addEventListener('mousedown', () => {
+        button.style.color = 'red';
+    }); 
+}
 
-// function logCopy(event) {
-//     alert('Text was coppied!')
-// }
-
-// allText.onCopy = logCopy();
+// change button text color back to white when mouse
+// is no longer hovering above the button
+let buttonsLeave = document.querySelectorAll('.btn');
+for(let buttonLeave of buttonsLeave) {
+    buttonLeave.addEventListener('mouseleave', () => {
+        buttonLeave.style.color = 'white';
+    });
+}
