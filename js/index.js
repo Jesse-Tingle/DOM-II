f98caa1fe958d78d4498ce02d5278abc4b1cec21
Jesse-Tingle <jtingle0@gmail.com>
2019-08-12 19:22:19 -0400

    add semi colons to end of statements

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@
 let header = document.querySelector('.logo-heading');
 header.addEventListener('mouseover', (event) => {
     event.target.style.backgroundColor = '#FDE45B';
-})
+});
 
 // increases the image size of the two images
 // in the content section
@@ -13,15 +13,15 @@ let images = document.querySelectorAll('.img-content');
 for (let image of images) {
     image.addEventListener('click', () => {
         image.style.width = '50%';
-    })
-}
+    });
+};
 
 // alerts a message notifying the user that 
 // the text was coppied to the clipboard
 let allText = document.querySelector('body');
 allText.addEventListener('copy', () => {
     alert('Text was coppied!');
-})
+});
 
 // change button text color to red on mousedown
 let buttons = document.querySelectorAll('.btn');
@@ -29,7 +29,7 @@ for(let button of buttons) {
     button.addEventListener('mousedown', () => {
         button.style.color = 'red';
     }); 
-}
+};
 
 // change button text color back to white when mouse
 // is no longer hovering above the button
@@ -38,7 +38,7 @@ for(let buttonLeave of buttonsLeave) {
     buttonLeave.addEventListener('mouseleave', () => {
         buttonLeave.style.color = 'white';
     });
-}
+};
 
 // changes the text color back to teal when the mouse pointer
 // goes over the button
@@ -46,11 +46,17 @@ let buttonsHover = document.querySelectorAll('.btn');
 for(let buttonHover of buttonsHover) {
     buttonHover.addEventListener('pointerover', () => {
         buttonHover.style.color = '#17A2B8';
-    })
-}
+    });
+};
 
 // plays audio when the page finishes loading
 var sound = new Audio('sound.mp3');
 sound.addEventListener('loadeddata', () => {
     sound.play();
+});
+
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'j'){
+        alert('The j key was pressed')
+    };
 });
